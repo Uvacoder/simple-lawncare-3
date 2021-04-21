bc03a8b121ba98eb8879565d7b100e8ffed84b0b
walidbelaabed <unknown.dza@gmail.com>
2021-04-21 01:04:38 +0100

    push the project

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+jQuery(document).ready(function($) {
+  $("#news-slider").owlCarousel({
+    items: 3,
+    itemsDesktop: [1199, 3],
+    itemsDesktopSmall: [980, 2],
+    itemsMobile: [600, 1],
+    navigation: true,
+    navigationText: ["", ""],
+    pagination: true,
+    autoplay:true,
+    autoplayTimeout:2300,
+    autoplayHoverPause:true,
+    loop:true
+  });
+
+  $("#testimonial-slider").owlCarousel({
+    items: 3,
+    itemsDesktop:[1000,3],
+    itemsDesktopSmall:[979,2],
+    itemsTablet:[768, 2],
+    itemsMobile:[650, 1],
+    pagination: true,
+    autoplay:true,
+    autoplayTimeout:2300,
+    autoplayHoverPause:true,
+    loop:true
+  });
+})
